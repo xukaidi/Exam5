7b234d9603eb626b8536a564ab4d8d071f19190d
330861322 <330861322@qq.com>
2015-08-29 21:51:43 +0800

    xukaidi

--- Exam5/ReadMe.txt
+++
@@ -1,3 +0,0 @@
-千万别mvn clean，clean会出错
-mvn compile
-mvn exec:java -Dexec.mainClass="com.hand.main.MainApp"
--- Exam5/src/main/java/com/imooc/mvcdemo/dao/FilmDAO.java
+++
@@ -1,8 +0,0 @@
-package com.imooc.mvcdemo.dao;
-
-public interface FilmDAO {
-	
-	
-	
-	  void create(String title, String des,Integer languagedi);
-}
